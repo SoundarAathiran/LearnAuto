7e7b2ced1184e2b5a89acac6fe097b123c7c6195
SoundarAathira <sounder.rla@gmail.com>
2022-11-06 20:33:03 +0530

    FirstTime

---
+++ src/test/java/com/git/GitLearn.java
@@ -0,0 +1,14 @@
+package com.git;
+
+public class GitLearn {
+
+	public void type() {
+		System.out.println("1");
+	}
+   public void click() {
+    System.out.println("2");
+}
+    public void select() {
+		System.out.println("3");
+	}
+}
