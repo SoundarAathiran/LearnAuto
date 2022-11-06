9a939cde29530f7cc0632f75bc2743fdb44b1254
SoundarAathira <sounder.rla@gmail.com>
2022-11-07 05:08:36 +0530

    method added

--- src/test/java/com/git/GitLearn.java
+++ src/test/java/com/git/GitLearn.java
@@ -4,11 +4,21 @@ public class GitLearn {
 
 	public void type() {
 		System.out.println("1");
-	}
+}
    public void click() {
     System.out.println("2");
 }
     public void select() {
 		System.out.println("3");
+}
+   public void test1() {
+	System.out.println("4");
+
+}
+    public void test2() {
+		System.out.println("5");
+	}
+    public void test3() {
+		System.out.println("6");
 	}
 }
